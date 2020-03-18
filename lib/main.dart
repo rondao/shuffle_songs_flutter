ef5af54c866d657672ad73fab6aacc69e04a102c
Rafael Rondao <rafael.rondao@gmail.com>
2020-03-18 11:36:31 -0300

    Refactor songsList components into screens folder
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
-import 'models/song.dart';
-import 'network/songsApi.dart' as songsApi;
 import 'screens/songsList.dart';
 
 void main() => runApp(ShuffleSongs());
@@ -22,50 +19,6 @@ class ShuffleSongs extends StatelessWidget {
         ),
       ),
       home: SongsList(),
-    );
-  }
-}
-
-class SongsList extends StatefulWidget {
-  @override
-  _SongsListState createState() => _SongsListState();
-}
-
-class _SongsListState extends State<SongsList> {
-  Future<List<Song>> _songs;
-
-  @override
-  void initState() {
-    _songs = songsApi.fetchSongs(http.Client());
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Shuffle Songs"),
-      ),
-      backgroundColor: Theme.of(context).primaryColorDark,
-      body: FutureBuilder<List<Song>>(
-        future: _songs,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) print(snapshot.error);
-
-          return snapshot.hasData
-              ? ListView.separated(
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (context, index) {
-                    return SongTile(snapshot.data[index].trackName,
-                        snapshot.data[index].primaryGenreName);
-                  },
-                  separatorBuilder: (context, index) {
-                    return SongsListDivider();
-                  },
-                )
-              : Center(child: CircularProgressIndicator());
-        },
-      ),
     );
   }
 }
