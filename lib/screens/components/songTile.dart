8ccd901d134e65d7f6d3221cbdd19dcbf3a1fef6
Rafael Rondao <rafael.rondao@gmail.com>
2020-03-20 21:45:21 -0300

    Add loading and error icons using cached package
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- lib/screens/components/songTile.dart
+++ lib/screens/components/songTile.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../models/song.dart';
@@ -11,7 +12,14 @@ class SongTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: CircleAvatar(backgroundImage: NetworkImage(_song.artworkUrl)),
+      leading: CachedNetworkImage(
+        imageUrl: _song.artworkUrl,
+        imageBuilder: (context, imageProvider) => CircleAvatar(
+          backgroundImage: imageProvider,
+        ),
+        placeholder: (context, url) => CircularProgressIndicator(),
+        errorWidget: (context, url, error) => Icon(Icons.cloud_off),
+      ),
       title: SongDescription(_song.trackName, _song.primaryGenreName),
     );
   }
