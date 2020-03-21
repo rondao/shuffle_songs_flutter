5dce4ed50f12381ca5263a9ea97c909954b84f6f
Rafael Rondao <rafael.rondao@gmail.com>
2020-03-21 12:54:39 -0300

    Refactor SongTile components into a single file
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- lib/screens/components/songTile.dart
+++ lib/screens/components/songTile.dart
@@ -2,7 +2,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../models/song.dart';
-import 'songDescription.dart';
 
 class SongTile extends StatelessWidget {
   final Song _song;
@@ -14,22 +13,59 @@ class SongTile extends StatelessWidget {
     return ListTile(
       title: Row(
         children: <Widget>[
-          CachedNetworkImage(
-            imageUrl: _song.artworkUrl,
-            imageBuilder: (context, imageProvider) => Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4),
-              child: CircleAvatar(
-                backgroundImage: imageProvider,
-                radius: 42,
-              ),
-            ),
-            placeholder: (context, url) => CircularProgressIndicator(),
-            errorWidget: (context, url, error) => Icon(Icons.cloud_off),
-          ),
+          SongArtwork(_song.artworkUrl),
           SizedBox(width: 12),
           SongDescription(_song.trackName, _song.primaryGenreName),
         ],
       ),
+    );
+  }
+}
+
+class SongArtwork extends StatelessWidget {
+  final String _artworkUrl;
+
+  SongArtwork(this._artworkUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: _artworkUrl,
+      imageBuilder: (context, imageProvider) => Padding(
+        padding: const EdgeInsets.symmetric(vertical: 4),
+        child: CircleAvatar(
+          backgroundImage: imageProvider,
+          radius: 42,
+        ),
+      ),
+      placeholder: (context, url) => CircularProgressIndicator(),
+      errorWidget: (context, url, error) => Icon(Icons.cloud_off),
+    );
+  }
+}
+
+class SongDescription extends StatelessWidget {
+  final String _description;
+  final String _title;
+
+  SongDescription(this._title, this._description);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          _title,
+          style: Theme.of(context).textTheme.headline5,
+        ),
+        SizedBox(height: 5),
+        Text(
+          _description,
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+      ],
     );
   }
 }
