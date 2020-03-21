f1afc6208b0c41b779a662a89e2d2e85231fa6c6
Rafael Rondao <rafael.rondao@gmail.com>
2020-03-20 21:23:20 -0300

    Show song art image
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- lib/screens/components/songTile.dart
+++ lib/screens/components/songTile.dart
@@ -1,23 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../../models/song.dart';
 import 'songDescription.dart';
 
 class SongTile extends StatelessWidget {
-  final String _description;
-  final String _title;
+  final Song _song;
 
-  SongTile(this._title, this._description);
+  SongTile(this._song);
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       // TODO: Change from Icon to CircleAvatar.
-      leading: Icon(
-        Icons.account_circle,
-        color: Colors.white70,
-        size: 48,
-      ),
-      title: SongDescription(_title, _description),
+      leading: Image.network(_song.artworkUrl),
+      title: SongDescription(_song.trackName, _song.primaryGenreName),
     );
   }
 }
