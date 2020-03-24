f23d7f91e3e8cb96c3334541ae72b7570c3776a0
Rafael Rondao <rafael.rondao@gmail.com>
2020-03-24 20:45:49 -0300

    Create test for fetch songs events
    Signed-off-by: Rafael Rondao <rafael.rondao@gmail.com>

--- test/songs_bloc_test.dart
+++ test/songs_bloc_test.dart
@@ -1,4 +1,37 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:shuffle_songs/bloc/songs_list_bloc.dart';
+import 'package:shuffle_songs/repository/songs_repository.dart';
 
-void main() { 
+import 'assets/songs_list_mock.dart';
+
+class MockSongsRepository extends Mock implements SongsRepository {}
+
+void main() {
+  MockSongsRepository mockSongsRepository;
+
+  setUp(() {
+    mockSongsRepository = MockSongsRepository();
+  });
+
+  group('SongsListEvent.fetchSongs', () {
+    test(
+      '(Without Bloc Test Library) Emits [SongsListLoading, SongsListReady] when successful',
+      () {
+        when(mockSongsRepository.fetchSongs())
+            .thenAnswer((_) async => mockSongsList);
+
+        final bloc = SongsListBloc(mockSongsRepository);
+        bloc.add(SongsListEvent.fetchSongs);
+
+        expectLater(
+          bloc,
+          emitsInOrder([
+            SongsListLoading(),
+            SongsListReady(mockSongsList),
+          ]),
+        );
+      },
+    );
+  });
 }
